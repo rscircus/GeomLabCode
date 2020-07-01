0c9a14af88816fd252b142d2fdcf430ed0f1e7c3
Roland Siegbert <roland@siegbert.info>
2020-07-01 12:00:19 +0200

    Add Label/TextInput to kivy

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 # Implementation of the GeomLab
 from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
 from kivy.uix.widget import Widget
 
 class SymbolicMaps(Widget):
