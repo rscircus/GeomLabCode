3af4f35d73c2418a5f54c60f06386b29719423ec
Roland Siegbert <roland@siegbert.info>
2020-07-01 12:15:26 +0200

    Add canvas for simple drawing

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
 import tkinter as tk
 import tkinter.ttk as ttk
 
+# Draw on canvas
+lastx, lasty = 0, 0
+
+def xy(event):
+    global lastx, lasty
+    lastx, lasty = event.x, event.y
+
+def addLine(event):
+    global lastx, lasty
+    canvas.create_line((lastx, lasty, event.x, event.y))
+    lastx, lasty = event.x, event.y
+
 # Main Frame
 class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
+        # Canvas
+        self.canvas = tk.Canvas()
+        self.canvas.pack(side='top', fill='both', expand='yes')
+        self.canvas.grid(column=0, row=0, sticky=(tk.N, tk.W, tk.E, tk.S))
+        self.canvas.bind("<Button-1>", xy)
+        self.canvas.bind("<B1-Motion>", addLine)
+
         self.master = master
-        self.pack()
+        #self.pack()
         self.create_widgets()
+
 
     def create_widgets(self):
 
@@ -25,6 +45,7 @@ class Application(tk.Frame):
         # TODO: Later orientation/gridding
         #self.algorithm.grid(column=0, row=0)
 
+
         # Quit Button
         self.quit = tk.Button(self, text="QUIT", fg="red",
                               command=self.master.destroy)
@@ -33,10 +54,14 @@ class Application(tk.Frame):
     def say_hi(self):
         print("hi there, everyone!")
 
+
+
 # Define window props
 window = tk.Tk()
 window.geometry('800x600')
 window.title('Symbolic Maps')
+window.columnconfigure(0, weight=1)
+window.rowconfigure(0, weight=1)
 
 # Menu
 menu = tk.Menu(window)
@@ -45,6 +70,11 @@ menu_item.add_command(label='Quit')
 menu.add_cascade(label='File', menu=menu_item)
 window.config(menu=menu)
 
-# Run application
+# Create application
 app = Application(master=window)
+
+# TODO: Ugly
+canvas = app.canvas #Canvas(root)
+
+# Run application
 app.mainloop()
