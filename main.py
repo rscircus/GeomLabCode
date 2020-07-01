debd5f22a46557d7664316eb7c914f19a4ff55a3
Roland Siegbert <roland@siegbert.info>
2020-07-01 12:02:11 +0200

    Switch from kivy to tkinter
    Basically, it's less hassle, less design overhead and more native.
    Hence, the switch. Leaving kivy in playground for potential later use.

--- main.py
+++ main.py
@@ -1,27 +1,50 @@
-# Implementation of the GeomLab
-from kivy.app import App
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput
-from kivy.uix.widget import Widget
+import tkinter as tk
+import tkinter.ttk as ttk
 
-class SymbolicMaps(GridLayout):
-    def __init__(self, **kwargs):
-        super(SymbolicMaps, self).__init__(**kwargs)
-        self.cols = 2
+# Main Frame
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack()
+        self.create_widgets()
 
-        self.add_widget(Label(text='User Name'))
-        self.username = TextInput(multiline=False)
+    def create_widgets(self):
 
-        self.add_widget(self.username)
-        self.add_widget(Label(text='password'))
+        # Hi there button
+        self.hi_there = tk.Button(self)
+        self.hi_there["text"] = "Hello World\n(click me)"
+        self.hi_there["command"] = self.say_hi
+        self.hi_there.pack(side="top")
 
-        self.password = TextInput(password=True, multiline=False)
-        self.add_widget(self.password)
+        # Combobox (to select algo, input data, cost function)
+        self.algorithm = ttk.Combobox(self)
+        self.algorithm['values'] = ("MinMxSumK", "Painter", "Random")
+        self.algorithm.current(1)
+        self.algorithm.pack(side="top")
+        # TODO: Later orientation/gridding
+        #self.algorithm.grid(column=0, row=0)
 
-class GeomLabApp(App):
-    def build(self):
-        return SymbolicMaps()
+        # Quit Button
+        self.quit = tk.Button(self, text="QUIT", fg="red",
+                              command=self.master.destroy)
+        self.quit.pack(side="bottom")
 
-if __name__ == '__main__':
-    GeomLabApp().run()
+    def say_hi(self):
+        print("hi there, everyone!")
+
+# Define window props
+window = tk.Tk()
+window.geometry('800x600')
+window.title('Symbolic Maps')
+
+# Menu
+menu = tk.Menu(window)
+menu_item = tk.Menu(menu)
+menu_item.add_command(label='Quit')
+menu.add_cascade(label='File', menu=menu_item)
+window.config(menu=menu)
+
+# Run application
+app = Application(master=window)
+app.mainloop()
