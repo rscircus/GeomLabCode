534c7c51371c705facf2a7531c739128c39430c6
Roland Siegbert <roland@siegbert.info>
2020-07-07 22:08:44 +0200

    Add black as src code formatter

--- noxfile.py
+++ noxfile.py
@@ -3,8 +3,16 @@ import nox
 
 locations = "src", "noxfile.py"
 
+
 @nox.session(python=["3.7"])
 def lint(session):
     args = session.posargs or locations
     session.install("flake8")
     session.run("flake8", *args)
+
+
+@nox.session(python="3.7")
+def black(session):
+    args = session.posargs or locations
+    session.install("black")
+    session.run("black", *args)
