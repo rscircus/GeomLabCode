758ab83598e83266eae153104042765a71098a77
David Göckede <david.goeckede@protonmail.com>
2020-09-12 15:19:08 +0200

    fix numerical error regarding special points on squares

--- src/geomlab/symbolicstacking.py
+++ src/geomlab/symbolicstacking.py
@@ -1695,8 +1695,8 @@ def calculateStepParam(a, b, c):
     w.append(c[1] - a[1])
 
     if(v[0]==0 and v[1]==0): return 0
-    elif(math.isclose(v[0]*w[1], v[1]*w[0])):
-        return w[0]/v[0] if v[0]!=0 else w[1]/v[1]
+    elif(abs(w[0]/v[0]-w[1]/v[1])<=0.02):
+        return (w[0]/v[0] + w[1]/v[1])/2
     else: return None
 
 
